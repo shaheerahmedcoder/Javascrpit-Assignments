0b315acdedf4a76176e481800efad19dcfeac5a1
shaheerahmedcoder <shaheerahmed.0206@gmail.com>
2024-08-24 00:14:19 +0500

    chapter no 1 and 2 solution

---
+++ app.js
@@ -0,0 +1,69 @@
+                     //Chapter No 01 : ALERT
+
+//Q1) Write a script to greet your website visitor using JS alert box?
+
+// alert("Hello Visitor!");
+
+//Q2) Write a script to display following message on your web page?
+
+// alert("Error! Please enter a valid password");
+
+//Q3) Write a script to display following message on your web?
+
+// alert("Welcome to JS Land" + "\nHappy Coding");
+
+//Q4) Write a script to diplay following messages in sequence:
+
+// alert("Welcome to JS Land");
+// alert("Happy Coding"+" \nPrevent this page from creating additional dialogs");
+
+//Q5)Generate the following message through browsers developers console:
+
+//Just write this code in the console..
+// alert("Hello...I can run JS through my web browser's console");
+
+//Q6,7,8 Script addition in a html file done already.
+
+               // Chapter No 01 ended!
+
+
+           //CHAPTER NO 02 : Variables for Strings
+
+//Q1) Declare a variable called username.
+// var username;
+
+// //Q2) Declare a variable called myNmae & assign to it a string that represents your Full Name.
+
+// var myName="Shaheer Ahmed";
+
+//Q3) Write script to 
+//a) Declare a JS variable, titled message.
+//b) Assign "Hello World" to variable message.
+//c) Display the message in a alert box.
+
+// var message="Hello World";
+// alert(message);
+
+//Q4) Write a script to save students bio data in JS variables and show the data in alert boxes.
+
+//  alert("Jhone Doe");
+//  alert("15 years old");
+//  alert("Certified Mobile Application Development");
+
+ //Q5) Write a script to display the following alert using one JS variable:
+
+//  alert("PIZZA\n"+"PIZZ\n"+"PIZ\n"+"PI\n"+"P");
+
+ //Q6) Declare a variable called email and assign to it a string that represents your Email Address.Show the mentioned message in a alert box.
+
+//  var email="shaheerahmed.0206@gamil.com";
+//  alert("My email address is "+email);
+
+ //Q7) Declare a variable called book and give it the value "A smarter way to learn JavaScript".Display the following message in alert box.
+
+//  var book="A smarter way to learn JavaScript";
+//  alert("I am trying to learn from the Book:\n"+book);
+
+//Q8) Done in index.html file.
+
+            //Chapter No 02 ended!
