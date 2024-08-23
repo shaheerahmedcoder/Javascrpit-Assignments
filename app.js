127f3eb00637137d185aba85c65342d5a7c274fb
shaheerahmedcoder <shaheerahmed.0206@gmail.com>
2024-08-24 01:04:00 +0500

    chapter 1,2,3 and 4 completed!

--- app.js
+++ app.js
@@ -64,6 +64,76 @@
 //  var book="A smarter way to learn JavaScript";
 //  alert("I am trying to learn from the Book:\n"+book);
 
-//Q8) Done in index.html file.
+//   Q8) Write a script to display this in browser through JavaScript 
+
+// console.log("Yah! I can write HTML content through JavaScript");
 
             //Chapter No 02 ended!
+
+            // Chapter No 03 : Variables for Numbers
+
+
+//Chapter No 03 : Variables for Numbers
+
+//Q1) Declare a variable called age and assign to it your age in an alert box.
+
+// var age=18;
+// alert("I am "+age+" years old");
+
+
+//Q2) Declare and intialize a variable to keep track of how many times a visitor has visited a web page.
+
+// var N_Times=14;
+// alert("you have visited this site "+N_Times+" times");
+
+
+//Q3) Declare a variable called birthtYear and assign it to your birth year.Show the following message in your browser:
+
+// var birthtYear=2006;
+// console.log("My birth year is "+birthtYear+"\nData type of my declared variable is number");
+
+//Q4)
+// var visitorName="Shaheer";
+// var quantity=7;
+// var product="tennis balls";
+// console.log(visitorName+" ordered "+quantity+" "+product+" on XYZ Store.");
+
+
+                // Chapter No 03  Ended!
+
+
+                // Chapter No 04 : Variable Names
+
+//Q1) Declare 3 variables in one statment.
+
+// var var1,var2,var3;
+
+//Q2) Declare 5 legal and 5 illegal variable names.
+
+//Legal Variable Names
+
+// var my_name;
+// var myname;
+// var $my;
+// var _my;
+// var myName;
+
+//Illegal Variable Names
+
+// var 1myname;
+// var @myname;
+// var -myname;
+// var .myname;
+// var %myname;
+
+//Q3) Display in your browser.
+
+// console.log("Rules for naming JS variables");
+// console.log("Variable names can only contain, numbers, $ and _. For example: $my_1stVariable");
+// console.log("Variable must begin with a letter, $ or _. For example: $name,_name or name");
+// console.log("Variable names are case sensitive");
+// console.log("Variable names should not be JS keywords");
+
+         // Chapter No 4 Ended
+
+         
