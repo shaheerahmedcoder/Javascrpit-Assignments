80a93a9338702dfe7ced0e9780277f23a141fc4f
shaheerahmedcoder <shaheerahmed.0206@gmail.com>
2024-09-26 20:10:09 +0500

    assignment

---
+++ Chapter Strings/app.js
@@ -0,0 +1,101 @@
+// console.log("Hello Strings");
+//spaces are also consider as string and their lengths does count.
+//indexes also starts from 0!
+//The reverse() method is then applied to reverse the order of the elements, and join() is used to combine the reversed elements back into a string
+// A string can be converted to an array with the split() method:
+
+//Q1
+// var firstName=prompt("Enter first name: ");
+// var lastName=prompt("Enter last name: ");
+// var fullName=firstName+" "+lastName;
+// console.log(fullName);
+
+
+//Q2  
+// var favouriteMobile=prompt("Favourite Phone Model is: ");
+// var length=favouriteMobile.length;
+// console.log(" My favourite phone is: "+ " "+favouriteMobile);
+// console.log("Length of String is:"+ length);
+
+
+
+//Q3
+// var a="Pakistani";
+// var b=a.indexOf("n");
+// console.log(b)
+
+
+//Q4
+// var a="Hello World";
+// console.log(a.lastIndexOf("l"));
+
+//Q5
+// var a="Pakistani";
+// console.log(a.charAt("3"));
+
+//Q6
+// var firstName="Shaheer";
+// var lastName="Ahmed";
+// var fullName=firstName.concat(" ",lastName);
+// console.log(fullName);
+
+
+//Q7
+// var a="Hyderabad";
+// var b=a.replace("Hyder","Islam");
+// console.log(b);
+
+//Q8
+// var message =" Ali and Sami are best friends. They play cricket and football together";
+// var result=message.replaceAll("and","&");
+// console.log(result);
+
+
+//Q9
+// var a="472";
+// console.log(a);
+// console.log(typeof(a));
+// var b=Number(a);
+// console.log(b);
+// console.log(typeof(b));
+
+// var a=["hello","world"];
+// console.log(a.join("    "));
+
+
+//Q10
+// var url=prompt("enter url: ");
+// console.log("URL: "+url);
+// var domain=url.replace("www.","");
+// console.log("Domain: "+domain);
+
+
+//Q11
+// var food=prompt("Enter food: ");
+// console.log(food.toUpperCase());
+
+
+//Q12
+// var sim=prompt("Enter sim: ");
+// console.log(sim.toLowerCase());
+
+//Q13
+// var lang="javascript";
+// console.log(lang.replace("j","J"));
+
+//Q14
+//split say gaya array mein join sayt houa string to number
+// console.log(num.split().join());
+// var num=35.36;
+// var a=num.toString();
+// console.log(a.replace(".",""));
+
+
+//Q15
+// var a=3;
+// var b=3;
+// var aa=a.toString();
+// var bb=b.toString();
+// console.log(aa.concat("",bb));
+
+
--- Loops/app.js
+++ Loops/app.js
@@ -23,11 +23,112 @@
 
 
 //Q5
-var fruits=["apple","banana","peach"];
-for(var i=0; i<fruits.length; i++){
-    console.log(fruits[i]);
+// var fruits=["apple","banana","peach"];
+// for(var i=0; i<fruits.length; i++){
+//     console.log(fruits[i]);
    
-}
-for(var i=0; i<fruits.length; i++){
-    console.log(`Element at index ${i} is ${fruits[i]}`)
-}
+// }
+// for(var i=0; i<fruits.length; i++){
+//     console.log(`Element at index ${i} is ${fruits[i]}`)
+// }
+
+
+//Q6
+// var array=[];
+// var N=prompt("Enter no of items you want to print: ");
+// console.log(`Number of items: ${N} , Items:`)
+// for(var i=0; i<N; i++){
+//     var item=prompt("Ënter item you want to store: ");
+//    console.log(item);
+
+// }
+
+
+//Q7
+// console.log("Counting: ");
+// for(var i=1; i<16; i++){
+//     console.log(i);
+// }
+// console.log("Reverse: ");
+// for(var j=10; j>=1; j--){
+//     console.log(j);
+    
+// }
+// console.log("Even: ");
+// for(var i=0; i<21; i++){
+//    if(i%2==0){
+//     console.log(i);
+    
+//    }
+    
+// }
+// console.log("Odd: ");
+// for(var i=1; i<21; i++){
+//     if(i%2!=0){
+//     console.log(i);}
+    
+// }
+// console.log("Series: ");
+// for(var i=2; i<21; i++){
+//     if(i%2==0){
+//     console.log(i+"k");
+// }   
+// }
+
+
+//Q8 wrong
+// var bakery=["cookie","bread","cake","pastry","chips"]
+
+// for(var i=0; i<bakery.length; i++){
+//     var search=prompt("search bakery item: ");
+//     if(search==bakery[i]){
+//         console.log(`${bakery[i]} is available at index ${i} in our bakery!`);
+//      }
+   
+    
+
+// }
+
+//Q9 wrong
+// var arr=[1,5,2,90,34]
+// for(var i=0; i<arr.length; i++){
+//     if(arr[0]>arr[1]){console.log(` Its is the largest number ${arr[0]}`);
+//     }
+//     if(arr[1]>arr[2]){console.log(` Its is the largest number ${arr[1]}`);
+//     }
+//     if(arr[2]>arr[3]){console.log(` Its is the largest number ${arr[2]}`);
+//     }
+//     if(arr[3]>arr[4]){console.log(` Its is the largest number ${arr[3]}`);
+//     }
+//     else{
+//         console.log(`Its is the largest number ${arr[4]}`);
+        
+//     }
+// }
+
+//Q10 11 leave
+
+//Q12
+// for(var i=5; i<=100; i++){
+//     if(i%5==0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+//Q16
+// for(var i=5; i>=0; i--){
+//     console.log(i,i-0.5);
+// }
+
+
+//Q19
+// for(var i=1; i<=7; i++){
+//     for(var j=1; j<=7; j++){
+//         console.log("*");
+        
+//     }
+// }
+
+
